281ae44993322cf73cb9060d67bef2e8b254390a
Rajesh G <92634862+Rajesh2723@users.noreply.github.com>
2022-09-17 08:11:40 +0530

    Update Number2.cpp

--- patterns/Number2.cpp
+++ patterns/Number2.cpp
@@ -21,4 +21,33 @@ int main()
     }
     
 }
+#####################another pattern
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int i=1;
+    int value=1;
+    while(i<=n){
+        int k=1;
+        while(k<=n-i){
+            cout<<" ";
+            k+=1;
+        }
+        int j=1;
+        while(j<=i){
+            cout<<value;
+            value+=1;
+            j+=1;
+            
+        }
+        cout<<endl;
+        i+=1;
+    }
+    
+}
 
