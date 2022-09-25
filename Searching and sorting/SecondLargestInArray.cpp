1fc0f008cd044f8581647f339d7258b464430f4f
Rajesh G <92634862+Rajesh2723@users.noreply.github.com>
2022-09-25 08:44:26 +0530

    Update SecondLargestInArray.cpp

--- Searching and sorting/SecondLargestInArray.cpp
+++ Searching and sorting/SecondLargestInArray.cpp
@@ -64,3 +64,32 @@ int findSecondLargest(int *input, int n)
     
     
 }
+##############################driver code
+#include <iostream>
+using namespace std;
+
+#include "solution.h"
+
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+
+		int size;
+		cin >> size;
+		int *input = new int[size];
+
+		for (int i = 0; i < size; i++)
+		{
+			cin >> input[i];
+		}
+
+		cout << findSecondLargest(input, size) << endl;
+
+		delete[] input;
+	}
+
+	return 0;
+}
