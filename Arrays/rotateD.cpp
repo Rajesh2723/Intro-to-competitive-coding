7ab87905a9fa0de6b1aaa95a72ee81cdb8091647
Rajesh G <92634862+Rajesh2723@users.noreply.github.com>
2022-10-28 19:17:39 +0530

    Create rotateD.cpp

---
+++ Arrays/rotateD.cpp
@@ -0,0 +1,29 @@
+/*
+Given an unsorted array arr[] of size N. Rotate the array to the left (counter-clockwise direction) by D steps, where D is a positive integer. 
+
+ 
+
+Example 1:
+
+Input:
+N = 5, D = 2
+arr[] = {1,2,3,4,5}
+Output: 3 4 5 1 2
+Explanation: 1 2 3 4 5  when rotated
+by 2 elements, it becomes 3 4 5 1 2.
+*/
+void rotate(int arr[],int start,int end){
+        while(start<=end){
+            int temp=arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
+            start++;
+            end--;
+        }
+    }
+    void rotateArr(int arr[], int d, int n){
+        d=d%n;
+        rotate(arr,0,d-1);
+        rotate(arr,d,n-1);
+        rotate(arr,0,n-1);
+    }
